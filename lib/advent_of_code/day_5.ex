452d7476d877b5b7316c28c43dab237a7ed2838c
Matthew Caldwell <caldwellysr@gmail.com>
2025-12-06 06:21:03 -0600

    Refactor, remove ranges and Enum.find calls in favor of tuples and pattern matching

--- lib/advent_of_code/day_5.ex
+++ lib/advent_of_code/day_5.ex
@@ -27,50 +27,26 @@ defmodule AdventOfCode.Day5 do
   def puzzle_two(data) do
     [valid_ids | _tests] = String.split(data, "\n\n")
 
-    valid_id_map =
-      valid_ids
-      |> String.split()
-      |> Enum.map(fn range ->
-        [start, finish] = String.split(range, "-")
+    valid_ids
+    |> String.split()
+    |> Enum.map(fn range ->
+      [start, finish] = String.split(range, "-")
+      {String.to_integer(start), String.to_integer(finish)}
+    end)
+    |> Enum.sort()
+    |> Enum.reduce([], fn {low, high}, acc ->
+      case acc do
+        [] ->
+          [{low, high}]
 
-        Range.new(String.to_integer(start), String.to_integer(finish))
-      end)
-      |> Enum.reduce([], fn %Range{first: low, last: high} = range, acc ->
-        intersect_low =
-          Enum.find(acc, fn %Range{first: first, last: last} ->
-            low >= first and low <= last
-          end)
+        [{prev_low, prev_high} | rest] when low <= prev_high + 1 ->
+          [{prev_low, max(prev_high, high)} | rest]
 
-        intersect_high =
-          Enum.find(acc, fn %Range{first: first, last: last} ->
-            high >= first and high <= last
-          end)
-
-        this_range_left =
-          Enum.filter(acc, fn %Range{last: last} ->
-            last < low
-          end)
-
-        this_range_right =
-          Enum.filter(acc, fn %Range{first: first} ->
-            first > high
-          end)
-
-        case {intersect_low, intersect_high} do
-          {nil, nil} ->
-            this_range_left ++ [range] ++ this_range_right
-
-          {%Range{} = x, nil} ->
-            this_range_left ++ [x.first..high] ++ this_range_right
-
-          {nil, %Range{} = y} ->
-            this_range_left ++ [low..y.last] ++ this_range_right
-
-          {%Range{} = x, %Range{} = y} ->
-            this_range_left ++ [x.first..y.last] ++ this_range_right
-        end
-      end)
-      |> Enum.map(&Range.size/1)
-      |> Enum.sum()
+        _ ->
+          [{low, high} | acc]
+      end
+    end)
+    |> Enum.map(fn {low, high} -> high - low + 1 end)
+    |> Enum.sum()
   end
 end
